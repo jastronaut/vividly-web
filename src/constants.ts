259042d9eb7268c28a5a08e14582cfb40039f13f
Jas Delgado <jas@jasdelgado.com>
2023-05-03 13:14:40 -0400

    extra slash lol

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const DEFAULT_AVATAR = `https://i.ibb.co/CnxM4Hj/grid-0-2.jpg`;
 export const uri =
 	process.env.VIVIDLY_API_URI ||
 	process.env.NEXT_PUBLIC_VIVIDLY_API_URI ||
-	'http://localhost:1337/v0/';
+	'http://localhost:1337/v0';
 
 export const URL_PREFIX =
 	process.env.VIVIDLY_API_URI ||
