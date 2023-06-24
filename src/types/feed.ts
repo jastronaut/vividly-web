834e5e536f111d26331cc828d54863d658434755
Jas Delgado <jas@jasdelgado.com>
2023-06-24 12:31:12 -0400

    attempt to fix feed sorting

--- src/types/feed.ts
+++ src/types/feed.ts
@@ -29,10 +29,26 @@ export function sortFeedFriendships(items: FeedFriendship[]): FeedFriendship[] {
 			return -1;
 		}
 
+		const isAUnread = a.lastReadPostTime < lastPostA.createdTime;
+		const isBUnread = b.lastReadPostTime < lastPostB.createdTime;
+
+		if (isAUnread && !isBUnread) {
+			return -1;
+		} else if (!isAUnread && isBUnread) {
+			return 1;
+		}
+
 		// rule 2: sort by post created time, descending
-		const timeDiff = lastPostB.createdTime - lastPostA.createdTime;
-		if (timeDiff !== 0) {
-			return timeDiff;
+		if (lastPostA.createdTime > lastPostB.createdTime) {
+			if (a.isFavorite !== b.isFavorite) {
+				return a.isFavorite ? -1 : 1;
+			}
+			return -1;
+		} else if (lastPostA.createdTime < lastPostB.createdTime) {
+			if (a.isFavorite !== b.isFavorite) {
+				return a.isFavorite ? -1 : 1;
+			}
+			return 1;
 		}
 
 		// rule 3: if equal, favorite comes first
@@ -40,19 +56,6 @@ export function sortFeedFriendships(items: FeedFriendship[]): FeedFriendship[] {
 			return a.isFavorite ? -1 : 1;
 		}
 
-		// rule 4: items with last read post after created time come last
-		const aLastReadTime = a.lastReadPostTime;
-		const bLastReadTime = b.lastReadPostTime;
-		const createdTime = lastPostA.createdTime;
-		if (aLastReadTime >= createdTime && bLastReadTime >= createdTime) {
-			return bLastReadTime - aLastReadTime;
-		} else if (aLastReadTime >= createdTime) {
-			return 1;
-		} else if (bLastReadTime >= createdTime) {
-			return -1;
-		}
-
-		// default: no change
 		return 0;
 	});
 }
