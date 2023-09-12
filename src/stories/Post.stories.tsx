49edeef5d0aa81e1be50531eacc5a78fd7e49d4c
Jas Delgado <jas@jasdelgado.com>
2023-09-12 09:36:55 -0400

    fix broken post stories page; remove unused component

--- src/stories/Post.stories.tsx
+++ src/stories/Post.stories.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { VividlyThemeProvider } from '../styles/Theme';
-import { Post } from '../components/common/Post';
+import { Content } from '@/components/post/Content';
 import { BlockType } from '@/types/post';
 
 export default {
-	title: 'Profile/Post',
-	component: Post,
-} as ComponentMeta<typeof Post>;
+	title: 'Post/Content',
+	component: Content,
+} as ComponentMeta<typeof Content>;
 
-const Template: ComponentStory<typeof Post> = args => (
+const Template: ComponentStory<typeof Content> = args => (
 	<VividlyThemeProvider>
-		<Post {...args} />
+		<Content {...args} />
 	</VividlyThemeProvider>
 );
 
 export const Primary = Template.bind({});
-
 Primary.args = {
+	postId: 1,
 	blocks: [
 		{
 			type: BlockType.TEXT,
@@ -29,6 +29,20 @@ Primary.args = {
 			url: 'https://i.imgur.com/B0VUfKl.jpg',
 			width: 316,
 			height: 316,
+		},
+	],
+};
+
+export const Link = Template.bind({});
+Link.args = {
+	postId: 1,
+	blocks: [
+		{
+			type: BlockType.LINK,
+			url: 'https://www.google.com',
+			title: 'Google',
+			description: 'Search engine',
+			imageURL: 'https://i.ibb.co/7CTbH64/google.png',
 		},
 	],
 };
