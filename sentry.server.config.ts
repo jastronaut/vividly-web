5701051cf7a743ed4d5a46d6aaa789359a1ba778
Jas Delgado <jas@jasdelgado.com>
2024-02-24 06:32:10 -0800

    sentry config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,14 +2,15 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://9df2f8afead34ab68bb3676ed2ff5bfb@o4505533712105472.ingest.sentry.io/4505547806277632",
+	dsn: 'https://9df2f8afead34ab68bb3676ed2ff5bfb@o4505533712105472.ingest.sentry.io/4505547806277632',
+	enabled: process.env.NODE_ENV === 'production',
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
 });
